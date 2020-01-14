3f6e0476b79e120884e78c45f8f9f720e559476d
Tracie Masek <42948851+traciemasek@users.noreply.github.com>
2020-01-14 09:51:46 -0500

    first commit

---
+++ skilled/index.js
@@ -0,0 +1,21 @@
+const User = {
+  init: function(name){
+    this.name = name
+    return this
+  },
+  getGreeting: function(){
+    return `Hello, ${this.name}`
+  }
+}
+
+
+
+var peter = User.init( "Peter" )
+
+console.log({peter})
+
+var greeting = peter.getGreeting()
+
+console.log( greeting )
+
+// Should output "Hello, Peter"
