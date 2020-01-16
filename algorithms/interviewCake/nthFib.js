df73fa2d20f386a0c144e87eeca0af83758a5d16
Tracie Masek <42948851+traciemasek@users.noreply.github.com>
2020-01-16 10:59:32 -0500

    nth fib bottom up

--- algorithms/interviewCake/nthFib.js
+++ algorithms/interviewCake/nthFib.js
@@ -2,20 +2,20 @@
 
 
 //this classic recursive method works fine but is O(2^n), which is exponential time and Not Good, Bob (see Binary Tree)
-// function fib(n) {
-//   //can't have a negative Fibonacci number
-//   if (n < 0) {
-//     throw new Error(
-//       'Index was negative. No such thing as a negative index in a series.');
-//   }
+function fib(n) {
+  //can't have a negative Fibonacci number
+  if (n < 0) {
+    throw new Error(
+      'Index was negative. No such thing as a negative index in a series.');
+  }
 
-//   //establish base case
-//   if(n === 0 || n === 1) {return n}
+  //establish base case
+  if(n === 0 || n === 1) {return n}
 
-//   console.log(`computing fib(${n})`);
-//   //recursive case
-//   return fib(n - 1) + fib(n - 2)
-// }
+  console.log(`computing fib(${n})`);
+  //recursive case
+  return fib(n - 1) + fib(n - 2)
+}
 
 // fib(5)
 
@@ -54,4 +54,32 @@ class Fibber {
 
 }
 
-new Fibber().fib(5)
+// new Fibber().fib(5)
+
+//or try solving from the bottom up
+
+function fib2(n) {
+  if (n < 0) {
+    throw new Error(`Negative index`);
+  } else if (n === 0 || n === 1) {
+    return n;
+  }
+
+  //need to track the previous 2 numbers at each step
+
+  let penultimate = 0;
+  let previous = 1;
+  let current;
+
+  for(let i = 1; i < n; i++) {
+    current = previous + penultimate
+    penultimate = previous
+    previous = current
+    console.log(current)
+  }
+
+  return current
+
+}
+
+fib2(10)
